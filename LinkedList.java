2d53a56cf19e4a983aedc1c3482d5c385084c2c6
Ariel <ariel.kerry@gmail.com>
2022-10-27 09:10:55 -0400

    initial commit

---
+++ LinkedList.java
@@ -0,0 +1,47 @@
+// Represents singly-linked list in java
+
+class LinkedList {
+
+	Node head;
+	Node tail;
+
+	LinkedList(Node headNode) {
+		head = headNode;
+		tail = headNode;
+	}
+
+	void prependNode(Node node) {
+		node.next = head;
+		head = node;
+	}
+
+	void appendNode(Node node) {
+		tail.next = node;
+		tail = tail.next;
+	}
+
+	void traverseAndPrint() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.value); // Print out value of node
+			if (current == head || current == tail) {
+				System.out.println(" <");
+			} else {
+				System.out.println();
+			}
+			current = current.next; // Move to next node
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList(new Node(2));
+		ll.appendNode(new Node(1));
+		ll.appendNode(new Node(3));
+		ll.traverseAndPrint();
+	}
+
+}
+
+// After code-along (two options):
+//	- Implement doubly-linked list and backwards traversal
+//	- 
---
+++ Node.java
@@ -0,0 +1,13 @@
+// Represents node for singly-linked list
+
+class Node {
+
+	int value;
+	Node next;
+
+	Node(int nodeValue) {
+		value = nodeValue;
+		next = null;
+	}
+	
+}
