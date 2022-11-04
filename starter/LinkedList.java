712316017d4a675d0ff682f8080b864bf7f06db6
Ariel <ariel.kerry@gmail.com>
2022-11-04 10:04:09 -0400

    added snake markdown file

--- starter/LinkedList.java
+++ starter/LinkedList.java
@@ -12,26 +12,30 @@ class LinkedList {
 	}
 
 	void prependNode(Node node) {
-		
+
 	}
 
 	void appendNode(Node node) {
-		
+		tail.next = node;
+		tail = tail.next;
 	}
 
 	void traverseAndPrint() {
-		
+		Node curr = head;
+		while (curr != null) {
+			System.out.println(curr.value + " -> ");
+			curr = curr.next;
+		}
 	}
 
 	public static void main(String[] args) {
 		// create a linked list (3 -> 2 -> 1 -> 2)
-		
+		LinkedList ll = new LinkedList(new Node(3));
+		ll.appendNode(new Node(2));
+		ll.appendNode(new Node(1));
+		ll.appendNode(new Node(2));
 		// traverse through and print
-
+		ll.traverseAndPrint();
 	}
 
 }
-
-// After code-along (two options) [10 mins]:
-//	- 
-//	- Implement doubly-linked list and backwards traversal
