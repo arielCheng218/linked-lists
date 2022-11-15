201a64360802708680118af5038d56b2b8070b3a
Ariel <ariel.kerry@gmail.com>
2022-11-15 17:31:17 -0500

    edited starter

--- starter/LinkedList.java
+++ starter/LinkedList.java
@@ -8,35 +8,26 @@ class LinkedList {
 	Node tail;
 
 	LinkedList(Node headNode) {
-		head = headNode;
-		tail = headNode;
+		
 	}
 
 	void prependNode(Node node) {
-
+		
 	}
 
 	void appendNode(Node node) {
-		tail.next = node;
-		tail = tail.next;
+		
 	}
 
 	void traverseAndPrint() {
-		Node curr = head;
-		while (curr != null) {
-			System.out.print(curr.value + " -> ");
-			curr = curr.next;
-		}
+		
 	}
 
 	public static void main(String[] args) {
 		// create a linked list (3 -> 2 -> 1 -> 2)
-		LinkedList ll = new LinkedList(new Node(3));
-		ll.appendNode(new Node(2));
-		ll.appendNode(new Node(1));
-		ll.appendNode(new Node(2));
+		
 		// traverse through and print
-		ll.traverseAndPrint();
+
 	}
 
 }
